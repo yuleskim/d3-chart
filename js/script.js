42e7cd74cde63aadaf6580cb5178059055137c94
Yuliya Kim <yk882@mail.missouri.edu>
2015-02-17 19:44:58 -0600

    added some new files

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = $(".chart").width() - margin.left - margin.right,
     height = $(".chart").height() - margin.top - margin.bottom;
 
-  //The lines of code above are defining the parameters of our baseball chart. 
+  //This is one variable. Margin is an object because of the curly braces. The comma eradicates the need to put a varibale for the objects width and height. Comma means that whatever variable we put next, we can put it under this one variable. The margin sets the parameters around the chart, not necesarrily the size of the chart.
 
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
@@ -21,8 +21,8 @@ var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left")
     .ticks(10)
-    .ticksFormat(function(d)  {
-      return d;
+    .tickFormat(function(d) {
+      return d
     });
 
     //We had to change the previous code from showing percentages to showing numbers. We do this by giving the ticks a format and function - meaning this is how the axes will be drawn. We are saying that the x axis will be oriented at the bottom and the y will be oriented at the left. 
@@ -36,13 +36,14 @@ var svg = d3.select(".chart").append("svg")
 
 d3.json("js/andre_ethier_card.json", function(error, data) {
   
+//Here we are applying attributes because we are in SVG. Translate is a lot like x and y but it is different. It is the way that you assign x and y position to groups. Margin x and margin y the left and the bottom, is the size of that diagonal line in the marginal convention.THe translate is the offset. 
 
-  // I believe here we are calling our json file so that we can chart the right data. I also understand that we are grouping something, but I'm not exactly sure what the attributes "transform" and "translate" are.
+//The Json is our ajax call. 
 
   x.domain(data.stats.map(function(d) { return d.year; }));
   y.domain([0, d3.max(data.stats, function(d) { return d.HR; })]);
 
-
+// Mapping is the method we are using for the categories within the domain. In the y domain we are returning the lowest and the highest numbers within the domain. (linear) Domain is part of the variable x.
 // Here we are assigning functions to the x and y on the chart. meaning that for the x domain we are showing the year from our json file and for our y domain we are showing the amount of homeruns. 
 
   svg.append("g")
@@ -66,12 +67,22 @@ d3.json("js/andre_ethier_card.json", function(error, data) {
       .data(data.stats)
     .enter().append("rect")
       .attr("class", "bar")
-      .attr("x", function(d) { return x(d.year); })
+      .attr("x", function(d) { 
+
+        console.log(x(d.year));
+
+        return x(d.year); })
+      
       .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.HR); })
+      .attr("y", function(d) { 
+
+        console.log(y(d.HR));
+
+        return y(d.HR); 
+      })
       .attr("height", function(d) { return height - y(d.HR); });
 
-      // The difference between this method and the $.each() method is that we are grouping all of the functions and attributes under the svg.selectAll. This simplifies/shortens our code.
+ // THis is where our bars are made. We are essentially selecting something that doesn't exist, but that we are filling with using our data.stats. Enter is to append a rectangle to this empty svg. When we console log that, we get the pixels x of all the bars.
 
 });
 
